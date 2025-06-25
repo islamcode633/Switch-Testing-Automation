a20ee3405bd0df4724d1e820ae335bccc2714a86
Islam <68608028+islamcode633@users.noreply.github.com>
2025-06-25 16:09:43 +0300

    Delete data.py

--- data.py
+++
@@ -1,8 +0,0 @@
-# If necessary, uncomment for use:
-
-IPV4_SWITCH = '192.168.127.253'
-IPV6_SWITCH = f'fe80::c1ff:fe81:3133%enp4s0'
-
-DEVICE_TYPE = 'cisco_ios'
-USERNAME = 'admin'
-PASSWORD = 'password'

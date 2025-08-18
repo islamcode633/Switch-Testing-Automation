6b94dcb97aace6710b47e180275fe564c82a3dc0
Islam Gashimov <68608028+islamcode633@users.noreply.github.com>
2025-08-18 20:31:08 +0300

    Add files via upload

---
+++ checking_lldp_support.py
@@ -0,0 +1,42 @@
+"""
+This module checks the support of the LLDP protocol in the switch
+"""
+
+from typing import (
+    List,
+    LiteralString,
+    Union
+)
+from subprocess import run
+from subprocess import CalledProcessError
+
+from data import RemoteConnection
+
+
+def get_lldp_from_client() -> Union[str, None]:
+    """ Returns info about Client [ interface, portDescr, mac and etc...] """
+    command: List[LiteralString] = 'sudo lldpcli show neighbors'.split()
+    try:
+        raw = run(command, capture_output=True, text=True, check=True, encoding='utf8')
+        return ' Client Info:\n' + raw.stdout
+    except CalledProcessError:
+        print('Command execution error')
+    except FileNotFoundError:
+        print(f'Command {command} not found')
+    return None
+
+
+def get_lldp_from_switch() -> Union[str, None]:
+    """ Returns info about Switch [ local interface, mac, portDescr and etc...] """
+    command: str = 'show lldp neighbors'
+    with RemoteConnection().init_conn_session() as ssh_conn:
+        return 'Switch Info:\n' + str(ssh_conn.send_command(command))
+
+
+def lldp_ctl() -> None:
+    """ Control of test execution flow """
+    client: Union[str, None] = get_lldp_from_client()
+    switch: Union[str, None] = get_lldp_from_switch()
+
+    if client and switch:
+        print(client + switch)
---
+++ ssh.py
@@ -0,0 +1,48 @@
+"""
+Checking SSH connection support on the Switch
+"""
+
+from data import RemoteConnection
+
+
+class SSHConnect():
+    """
+    Provides services for activation/deactivation
+    and verification of connections via ssh.
+    """
+    @staticmethod
+    def is_enabled(output: str) -> bool:
+        """ Checking ssh connection activity """
+        can_active: str = 'enabled'
+        return can_active in output
+
+    @staticmethod
+    def get_info_about_ssh(tn_connect) -> str:
+        """ Get information about the presence of a connection via ssh """
+        mode: str = 'do show ip ssh'
+        output: str = tn_connect.send_command(mode).split()
+        return output
+
+    @staticmethod
+    def activate(tn_connect) -> None:
+        """ Activate ssh connection """
+        enable_ssh_connection: list[str] = ['ip ssh']
+        tn_connect.send_config_set(enable_ssh_connection)
+
+    @staticmethod
+    def deactivate(tn_connect) -> None:
+        """ Deactivate ssh connection """
+        disable_ssh_connection: list[str] = ['no ip ssh']
+        tn_connect.send_config_set(disable_ssh_connection)
+
+
+def ssh_ctl():
+    """ Control of test execution flow """
+    with RemoteConnection(type_conn='telnet').init_conn_session() as tn_connect:
+        output: str = SSHConnect.get_info_about_ssh(tn_connect=tn_connect)
+        reuslt: bool = SSHConnect.is_enabled(output=output)
+        if not reuslt:
+            SSHConnect.activate(tn_connect=tn_connect)
+
+    with RemoteConnection().init_conn_session() as ssh_connect:
+        print(ssh_connect.send_command('show ip interface'))
